fa7102227a3a514608ebcbd9921ead2113e3acb4
Brian Ngezmoe <bngezmoe@gmail.com>
2026-04-21 00:33:02 +0200

    Feat : Added Doctor and Club Dashboard , Backend intergration

--- KhoiIntegration/obj/Debug/net10.0/khoiIntegration.AssemblyInfo.cs
+++ KhoiIntegration/obj/Debug/net10.0/khoiIntegration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("khoiIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b330af98472905cba18dadfe2c64d784085f7df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("khoiIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("khoiIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
