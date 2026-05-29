936ed4d46ebfed408ba1d81c08df11a60aa2c00a
thatospec <thatobulane0@gmail.com>
2026-05-29 19:05:57 +0200

    Updateadmin dashboard UI

--- Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e4437e4847f244e0d635890c83b86bd73af16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874ef5600bbff830a51c58e7b4b513c1f6754ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
